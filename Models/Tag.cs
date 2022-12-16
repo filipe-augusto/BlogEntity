e4a02ac266ee215367129c46ba36c2e6f49791e0
Filipe Augusto Santos Chaves <fchaves@institutomonitor.com.br>
2022-12-16 16:29:33 -0300

    Configuration dataContext

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 namespace blog.Models;
 
+[Table("Tag")]
 public class Tag{
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
     public string Slug { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // dotnet tool install --global dotnet-ef
-//dotnet add package Microsoft.EntityFrameWork
+//dotnet add package Microsoft.EntityFrameWork.SqlServer
+using blog.Models;
+using Blog.Data;
+
+using (var context = new BlogDataContext()){
+        var tag = new Tag{Name="ASP.NET", Slug="aspnet"};
+        context.Tags.Add(tag);
+        context.SaveChanges();//salva no banco
+
+}
