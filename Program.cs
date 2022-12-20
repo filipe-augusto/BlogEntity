c540c3566af7d3c23ebfd74761015259d2d379f2
Filipe Augusto Santos Chaves <faugusto.chaves@gmail.com>
2022-12-20 09:57:07 -0300

    about tolist

--- Program.cs
+++ Program.cs
@@ -17,9 +17,20 @@ using (var context = new BlogDataContext()){
         // context.Update(tag);
         // context.SaveChanges();
         //obs sempre que precisar atualizar ou deletar precisa resgatar o objeto do banco
+       
         //DELETE
         // var tag = context.Tags.FirstOrDefault(x => x.Id ==  3);
         // context.Remove(tag);
         // context.SaveChanges();
 
+       // var tags = context.Tags.ToList();//sempre que tiver um .tolist estou executando a query
+        //ERRADO
+        var tags_ERRADO = context.Tags.ToList().Where(x=>x.Name.Contains(".NET"));
+        // Da forma acima ele consuta todas as tags  e depois aplica o filtro
+
+        //CERTO - O TOLIST SEMPRE SEMPRE SERÁ O ULTIMO
+           var tags_CERTO = context.Tags.Where(x=>x.Name.Contains(".NET")).ToList();
+        // foreach(var tag in tags){
+        //     Console.WriteLine(tag.Name);
+        // }
 }
